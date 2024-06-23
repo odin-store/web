b03c0f3123d5bb1530a4c37ef4e2acff7a3ef434
Lee Gyu Yeon <55003317+damie824@users.noreply.github.com>
2024-06-24 00:13:13 +0900

    🚨 신고 페이지 추가

--- src/app/(utils)/report/page.tsx
+++ src/app/(utils)/report/page.tsx
@@ -3,28 +3,40 @@
 import { useSearchParams } from "next/navigation";
 import { ReportSelector } from "@/components/report/selector";
 import Header from "@/components/global/header";
-import { useState } from "react";
+import { useState, useEffect, Suspense } from "react";
 
-export default function ReportPage() {
+interface SearchParamsComponentProps {
+  setType: (type: string) => void;
+}
+
+function SearchParamsComponent({ setType }: SearchParamsComponentProps) {
   const searchParams = useSearchParams();
-  const [curretType, setType] = useState("");
   const type = searchParams.get("type");
 
-  useState(() => {
+  useEffect(() => {
     if (type) {
       setType(type);
     }
   });
 
+  return null;
+}
+
+export default function ReportPage() {
+  const [curretType, setType] = useState("");
+
   return (
     <main className="max-w-[1200px] mx-auto">
       <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SearchParamsComponent setType={setType} />
+      </Suspense>
       <div className="flex mx-20 justify-between items-end mt-10">
         <h1 className="text-5xl font-bold">
           {curretType === "bugbounty" ? "Bug Bounty" : "Report"}
         </h1>
         <ReportSelector
-          {...(type ? { defaultValue: type } : {})}
+          {...(curretType ? { defaultValue: curretType } : {})}
           onValueChange={(value) => {
             setType(value);
           }}
